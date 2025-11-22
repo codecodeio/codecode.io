f3175797e9d4ebb48e15231b507fa1733b348cff
codecodeio <matt@codecode.io>
2025-11-22 09:35:50 -0500

    Add contact to general segment during subscribe.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,7 @@ import { ActionError, defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
 import { Resend } from "resend";
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
+const segmentIdGeneral = 'b196b0c9-b1d4-4ade-b89e-288c80d754b5';
 
 export const server = {
   subscribe: defineAction({
@@ -9,22 +10,39 @@ export const server = {
     input: z.object({
       email: z.string().email(),
     }),
-    handler: async (input) => {      
-      const { data, error } = await resend.contacts.create({
+    handler: async (input) => {     
+      // Add contact to Resend 
+      const { data: createData, error: createError } = await resend.contacts.create({
         email: input.email,
         unsubscribed: false,
       });     
 
-      if (error) {
-        console.error('Resend error:', error);
+      if (createError) {
+        console.error('Resend contacts.create error:', createError);
         throw new ActionError({
           code: "BAD_REQUEST",
-          message: error.message,
+          message: createError.message,
         });
       }
 
-      console.log('Resend response data:', data);
-      return data;
+      console.log('Resend contacts.create response data:', createData);
+
+      //Add contact to segment
+      const { data: segmentData, error: segmentError } = await resend.contacts.segments.add({
+        email: input.email,
+        segmentId: segmentIdGeneral,
+      });
+
+      if (segmentError) {
+        console.error('Resend contact.segments.add error:', segmentError);
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: segmentError.message,
+        });
+      }
+
+      console.log('Resend contact.segments.add response data:', segmentData);
+      return segmentData;
     }
   })
 }
