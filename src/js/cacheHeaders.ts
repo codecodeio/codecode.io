13a7f898ec0a8f8c94611a98f4b13c0d041d761d
codecodeio <matt@codecode.io>
2025-11-22 00:03:33 -0500

    Capitalize the headers in case that is an issue.

--- src/js/cacheHeaders.ts
+++ src/js/cacheHeaders.ts
@@ -5,10 +5,10 @@ interface CacheHeadersContext {
 
 export function setCacheHeaders(Astro: CacheHeadersContext, cacheTag?: string): void {
   // The browser should always check freshness
-  Astro.response.headers.set("cache-control", "public, max-age=0, must-revalidate");
+  Astro.response.headers.set("Cache-Control", "public, max-age=0, must-revalidate");
   // The CDN should cache for a year, but revalidate if the cache tag changes
-  Astro.response.headers.set("netlify-cdn-cache-control", "s-maxage=31536000");
+  Astro.response.headers.set("Netlify-CDN-Cache-Control", "public, s-maxage=31536000, stale-while-revalidate=86400");
   // Tag the page with a cache tag
   const tag = cacheTag ?? Astro.url.pathname.replace(/^\/|\/$/g, "");
-  Astro.response.headers.set("netlify-cache-tag", tag);
+  Astro.response.headers.set("Netlify-Cache-Tag", tag);
 }
