a229fec56d23f89ea112824b83e295f8fcda84bd
codecodeio <matt@codecode.io>
2025-11-25 22:54:44 -0500

    Add durable cache header.

--- src/js/cacheHeaders.ts
+++ src/js/cacheHeaders.ts
@@ -7,7 +7,7 @@ export function setCacheHeaders(Astro: CacheHeadersContext, cacheTag?: string): 
   // The browser should always check freshness
   Astro.response.headers.set("Cache-Control", "public, max-age=0, must-revalidate");
   // The CDN should cache for a year, but revalidate if the cache tag changes
-  Astro.response.headers.set("Netlify-CDN-Cache-Control", "public, s-maxage=31536000, stale-while-revalidate=86400");
+  Astro.response.headers.set("Netlify-CDN-Cache-Control", "public, durable, s-maxage=31536000, stale-while-revalidate=86400");
   // Tag the page with a cache tag
   const tag = cacheTag ?? Astro.url.pathname.replace(/^\/|\/$/g, "");
   Astro.response.headers.set("Netlify-Cache-Tag", tag);
